b1cc6295fb549c90f0e50733229d3aca2810663d
Arnaldo Pedroso <pedroso.arnaldo@gmail.com>
2017-04-15 19:38:11 -0300

    .

---
+++ F1_Gambling/F1_Gambling/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  F1_Gambling
+//
+//  Created by Arnaldo Pedroso on 4/15/17.
+//  Copyright © 2017 Arnaldo Pedroso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ F1_Gambling/F1_Gambling/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  F1_Gambling
+//
+//  Created by Arnaldo Pedroso on 4/15/17.
+//  Copyright © 2017 Arnaldo Pedroso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
